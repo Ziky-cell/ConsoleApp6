6dc7f1efeca18276347dc054f980cfba07633734
Ziky-cell <va.fa.94@inbox.ru>
2022-02-10 14:48:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/IQiueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal interface IQueue
+    {
+        int Peek();
+        int Dequeue();
+        void Enqueue(int item);
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Programm
+    {
+        static void Main()
+        {
+            Queue yt = new(); 
+
+            yt.Enqueue(6);
+            yt.Enqueue(12);
+            yt.Enqueue(24);
+            yt.Enqueue(48);
+
+            yt.Dequeue();
+            yt.Dequeue();
+            yt.Dequeue();
+            yt.Dequeue();
+
+            Console.WriteLine(yt.Peek());
+        }
+    }
+}
